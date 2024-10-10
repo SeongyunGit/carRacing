522207180628be2f1b774ae025712eb828e0a15e
Seongyun <okks3579@gmail.com>
2024-10-10 23:22:33 +0900

    refactor: 예외 처리 수정 실패

--- src/main/java/racingCar/Application.java
+++ src/main/java/racingCar/Application.java
@@ -4,7 +4,8 @@ public class Application {
     public static void main(String[] args) {
         CarModel model = new CarModel();
         CarView view = new CarView(model);
-        Car car = new Car();
+        String name = "";
+        Car car = new Car(name);
         CarController controller = new CarController(view,car);
         controller.gameStart();
     }
--- src/main/java/racingCar/Car.java
+++ src/main/java/racingCar/Car.java
@@ -3,16 +3,20 @@ package racingCar;
 import java.util.List;
 
 public class Car {
-//    private final String name;
-//    private int position = 0;
-//
-//    public Car() {
-//        this.name = name;
-//    }
+    private String name;
+    private int position = 0;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 5글자 이하여야 한다.");
+        }
+        this.name = name;
+    }
     // 추가 기능 구현
+
     public static void nameCountNumber(List<String> nameList) {
-        for (int i=0;i< nameList.size();i++) {
-            if (nameList.get(i).length()>5) {
+        for (String name : nameList) {
+            if (name.length()>5) {
                 throw new IllegalArgumentException("[ERROR] 이름은 5글자 이하여야 한다.");
             }
         }
